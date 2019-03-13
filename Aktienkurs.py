1b8fce4e024f78727d4a5df3976019df3fffda5e
Dean <deanvdmerwe@hotmail.com>
2019-03-13 09:55:11 +0100

    Step 3

--- Aktienkurs.py
+++ Aktienkurs.py
@@ -18,11 +18,14 @@ print("test edit")
 df.to_csv('TSLA.csv')
 
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+print(df.head())
 
-df.plot()
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
-
-df['Adj Close'].plot()
-plt.show()
-
-df[['High','Low']]
