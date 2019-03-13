fa66c4cbda86118126aeffedcc85ace2ee35a2f7
Flavio <flavio.hartmann@student.unisg.ch>
2019-03-13 09:37:50 +0100

    Changed SIN to TSLA

--- Aktienkurs.py
+++ Aktienkurs.py
@@ -9,5 +9,6 @@ style.use('ggplot')
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2016,12,31)
 
-df = web.DataReader('SIN', 'yahoo', start, end)
+df = web.DataReader('TSLA', 'yahoo', start, end)
 print(df.head(20))
+
