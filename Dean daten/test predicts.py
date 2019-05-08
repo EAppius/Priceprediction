9d7ff379370e3991bf210078d9be37db64ca3ba8
Dean <deanvdmerwe@hotmail.com>
2019-05-08 15:34:44 +0200

    set regressors

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -46,3 +46,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
+
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+regressor.fit(X_train, y_train, epochs = 50, batch_size = 32)
