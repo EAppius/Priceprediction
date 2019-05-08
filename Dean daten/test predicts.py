0c5c44625519821cc6458b5304e464eecea4c8ca
Dean <deanvdmerwe@hotmail.com>
2019-05-08 15:29:52 +0200

    create data structure

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -26,3 +26,11 @@ print(training_set)
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(10, 200):
+    X_train.append(training_set_scaled[i-10:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
