646e5a2420dc9ebb76cf52a9528a9c253c1bb1c5
Dean <deanvdmerwe@hotmail.com>
2019-05-08 15:27:51 +0200

    added path and changed variables

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -12,11 +12,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-dataset = pd.read_csv(".csv", delimiter = ";")
+dataset = pd.read_csv("/content/drive/My Drive/IC Tech/IC Test/sp500_joined_closes_correct.csv", delimiter = ";")
 print(dataset)
-y = dataset.iloc[:,0]
-X = dataset.iloc[:,1]
-dataset = dataset[dataset[0].notnull() & dataset[DATA_COLUMN].notnull()]
+#y = dataset.iloc[:,0]
+#X = dataset.iloc[:,1]
+#dataset = dataset[dataset[0].notnull() & dataset[DATA_COLUMN].notnull()]
 
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 training_set, dataset_test = train_test_split(dataset.iloc[:,[0,DATA_COLUMN]], test_size = 0.25, shuffle=False)
