4be5958ac65e5eba8b344e55e6d6fa18b78ef54f
Dean <deanvdmerwe@hotmail.com>
2019-05-08 15:28:50 +0200

    feature scale

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 
 dataset = pd.read_csv("/content/drive/My Drive/IC Tech/IC Test/sp500_joined_closes_correct.csv", delimiter = ";")
-print(dataset)
+#print(dataset)
 #y = dataset.iloc[:,0]
 #X = dataset.iloc[:,1]
 #dataset = dataset[dataset[0].notnull() & dataset[DATA_COLUMN].notnull()]
@@ -21,3 +21,8 @@ print(dataset)
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 training_set, dataset_test = train_test_split(dataset.iloc[:,[0,DATA_COLUMN]], test_size = 0.25, shuffle=False)
 print(training_set)
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
