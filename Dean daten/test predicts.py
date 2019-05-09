140c9b60a7a8175880bde713e6d907e0bd98825f
Dean <deanvdmerwe@hotmail.com>
2019-05-09 16:38:26 +0200

    fixed

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -88,9 +88,8 @@ inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 X_test = []
 for i in range(60, 80):
-X_test.append(inputs[i-60:i, 0])
+    X_test.append(inputs[i-60:i, 0])
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
