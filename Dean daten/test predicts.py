d8817d8ffe2e2fbff0f0f47f08176a3cfa0514aa
Dean <deanvdmerwe@hotmail.com>
2019-05-09 16:18:30 +0200

    WIP of p3

--- Dean daten/test predicts.py
+++ Dean daten/test predicts.py
@@ -85,5 +85,12 @@ real_stock_price = dataset_test.iloc[:,[0,DATA_COLUMN]].values
 dataset_total = dataset.iloc[:,[0,DATA_COLUMN]]
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
-inputs = sctransform(inputs)
+inputs = sc.transform(inputs)
 X_test = []
+for i in range(60, 80):
+X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
