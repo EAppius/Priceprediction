5ddf1559f801380452041580e254d8fd4ba2fe55
Dean <deanvdmerwe@hotmail.com>
2019-05-18 17:34:11 +0200

    Added all files from the historical data extraction and deleted some files that are no longer needed for our project, since they were merely notes. And archived other files that might still be a good reference for later

---
+++ Archiv/Colab und sonstige daten/python tutorial.py
@@ -0,0 +1,7 @@
+#Exercise file for python tutorial 29.3.2019
+print("hello world")
+
+cards = 110
+price_per_card = 14.15
+total_cost = cards * price_per_card
+print ("I payed only " + str(total_cost) + " CHF for all " + str(cards) + " cards!")
